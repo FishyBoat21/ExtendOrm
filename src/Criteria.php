36031b944b92f22a90571639ff81207c215cbe31
Joseph <tembok511@gmail.com>
2025-08-25 19:41:31 +0700

    alpha.4.3.Joseph Chou.Fix criteria to initilaize criterion

--- src/Criteria.php
+++ src/Criteria.php
@@ -1,7 +1,7 @@
 <?php
 namespace FishyBoat21\ExtendOrm;
 class Criteria{
-    public array $Criterion;
+    public array $Criterion = [];
     public function Add(Criterion $criterion){
         $this->Criterion[] = $criterion;
         return $this;
